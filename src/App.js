959c4ef8fbd1fb3347419a6571295e03e8994a68
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2021-11-14 08:50:23 -0700

    login and logout kinda works

--- src/App.js
+++ src/App.js
@@ -12,6 +12,66 @@ import {
 import NavigationBar from './Components/Header/NavigationBar';
 import './App.css'
 
+import 'firebase/auth';
+
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from '@firebase/auth';
+import { getAuth, signOut, onAuthStateChanged } from '@firebase/auth';
+import { initializeApp } from '@firebase/app';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { getFirestore, updateDoc, addDoc,collection } from '@firebase/firestore'
+
+
+const firebaseApp = initializeApp({ 
+    apiKey: "AIzaSyCV6lWSaS3VpPALnuj_nZ6oGnsBG1_RM-g",
+      authDomain: "level-7f535.firebaseapp.com",
+      databaseURL: "https://level-7f535-default-rtdb.firebaseio.com/",
+      projectId: "level-7f535",
+      storageBucket: "level-7f535.appspot.com",
+      messagingSenderId: "600278227779",
+      appId: "1:600278227779:web:012ba071acccfe0ac12df6",
+      measurementId: "G-NS3XXMZYGC"
+});
+
+const firestore = getFirestore();
+const auth = getAuth();
+
+const userCollection = collection(firestore, 'users 1');
+
+async function updateUsers(username) {
+  const updatedUser = await updateDoc(userCollection, {name: username});
+}
+
+function addNewUser(userName) {
+  const newUser = addDoc(userCollection, {name: userName});
+}
+
+function checkForSignIn()
+{
+    
+    const user = auth.currentUser;
+    
+    const provider = new GoogleAuthProvider();
+    
+    if (user)
+    {
+      console.log("You are already SIGNED IN!");
+    }
+    else
+    {
+      signInWithPopup(auth, provider).then((result) => {
+        const user = result.currentUser;
+        const uid = user.uid;
+        addNewUser(uid);
+        
+        
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+}
+
+
+
 class App extends React.Component {
   constructor() {
     super()
@@ -24,6 +84,7 @@ class App extends React.Component {
 
   handleSignIn() {
     console.log("handleSignIn")
+    checkForSignIn();
     this.setState({       // ROBERTO: here you want to check if the user is signed in or not, if they are, set isSignedIn to true
       isSignedIn: true
     })
@@ -31,6 +92,7 @@ class App extends React.Component {
 
   handleSignOut() {
     console.log("handleSignOut")
+    signOut(auth);
     this.setState({       // ROBERTO: here you want to check if the user is signed in or not, if they are, set isSignedIn to true
       isSignedIn: false
     })
