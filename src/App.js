02bd161baa5a101a5313c9809b98070856ff2d3a
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2021-11-13 18:21:40 -0700

    imported-dependencies

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Header from './Components/Header'
 import FormContainer from './Components/FormContainer'
 import Footer from './Components/Footer'
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/store';
+
 function App() {
   return (
     <div className="App">
